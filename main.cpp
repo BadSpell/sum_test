dafb5aa0de48df4f51867b6667673d1cc97b58c3
BadSpell <public4d@gmail.com>
2017-07-05 18:52:38 +0900

    Modify main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 // main.cpp
-#include <iostream>
+#include <stdio.h>
 #include "sum.h"
-using namespace std;
 
 int main() {
-  int n;
-  cin >> n;
-  cout << sum(n) << endl;
-  return 0;
+	int s = sum(100);
+	printf("%d\n", s);
+	return 0;
 }
